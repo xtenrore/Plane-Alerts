b8fd7e6880a997ab3758551f943f9faffe4c4f48
xtenrore <xtenrore@gmail.com>
2026-09-25 22:18:01 +0300

    Pin v5.6.10 release identity

--- tests/test_user_experience_v54.py
+++ tests/test_user_experience_v54.py
@@ -34,7 +34,7 @@ def _base() -> dict:
 
 
 def test_release_identity_and_physical_predictor_are_pinned():
-    assert VERSION == "5.6.9"
+    assert VERSION == "5.6.10"
     assert PREDICTION_VERSION == "5.3-3d-proximity-age-aware"
 
 
