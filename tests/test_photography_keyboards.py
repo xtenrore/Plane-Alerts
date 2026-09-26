b44497172546c4e5d540730575d75a0a0215753a
xtenrore <xtenrore@gmail.com>
2026-09-26 15:32:30 +0300

    Update alert tracker regression for v5.8.0

--- tests/test_photography_keyboards.py
+++ tests/test_photography_keyboards.py
@@ -9,9 +9,9 @@ def test_alert_keyboard_has_photo_and_feedback_actions():
     assert kb.inline_keyboard[1][1].callback_data.startswith("fb:dis:")
 
 
-def test_alert_keyboard_restores_adsbfi_button_when_icao_is_known():
+def test_alert_keyboard_uses_flightradar24_fallback_when_only_icao_is_known():
     kb = notification_actions_keyboard("notif123", "4BAA01")
     assert len(kb.inline_keyboard) == 3
-    assert kb.inline_keyboard[0][0].text == "Open in ADSB.fi"
-    assert kb.inline_keyboard[0][0].url == "https://globe.adsb.fi/?icao=4baa01"
+    assert kb.inline_keyboard[0][0].text == "Open in Flightradar24"
+    assert kb.inline_keyboard[0][0].url == "https://www.flightradar24.com/data"
     assert kb.inline_keyboard[1][0].callback_data == f"{PHOTO_CALLBACK_PREFIX}notif123"
